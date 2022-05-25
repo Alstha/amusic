4187a7625613be251a70cc73ac1c3b48916e2652
Alstha <88321011+Alstha@users.noreply.github.com>
2022-05-25 16:52:50 +0545

    Update script.js

--- winter/script.js
+++ winter/script.js
@@ -9,14 +9,14 @@ function Myfuc(){
 img.onclick= function(){
     if (audio.paused) {
         audio.play();
-        img.src="img/pause.png";
+        img.src="winter/img/pause.png";
         img.style.boxShadow="-10px -10px 22px rgb(206, 49, 75)";
         gif.style.display="block";
         audio.style.display="block";
     }
     else{
         audio.pause();
-        img.src="img/play.jpg";
+        img.src="winter/img/play.jpg";
         img.style.boxShadow="10px 10px 22px rgb(107, 206, 49),-10px -10px 22px rgb(107, 206, 49)";
         gif.style.display="none";
         audio.style.display="none";
